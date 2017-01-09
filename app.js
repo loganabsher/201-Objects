4d4c181d5ab4ca4136cb14f48f45476e39446dc2
Logan Absher <absherlogan@gmail.com>
2017-01-09 13:52:15 -0800

    set up inital objects and basic html

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 'use strict';
+
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avrg: 6.3,
+};
+
+var seaTacAirport = {
+  minCust: 3,
+  maxCust: 24,
+  avrg: 1.2,
+};
+
+var seattleCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avrg: 3.7,
+};
+
+var capitolHill = {
+  minCust: 20,
+  maxCust: 38,
+  avrg: 2.3,
+};
+
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avrg: 4.8,
+};
